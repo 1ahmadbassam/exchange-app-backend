0e55389244d05f8062eff8555aab384589ecd219
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2025-04-12 21:30:35 +0300

    add /exchangeRate/volatility, fix broken month cache logic, format files

--- api/exchange.py
+++ api/exchange.py
@@ -8,7 +8,8 @@ from init import limiter, db
 from model.rate import DailyRate, MonthlyRate
 from model.transaction import Transaction
 
-MSG = "The {} exchange rate has {} from {} to {} by a factor of {}% over the past {}."
+TREND_MSG = "The {} exchange rate has {} from {} to {} by a factor of {}% over the past {}."
+VOLATILITY_MSG = "The {} exchange rate varied {} and ranged between {} and {} over the past {}. "
 
 exchange_bp = Blueprint('exchange', __name__)
 
@@ -31,21 +32,24 @@ def _get_exchange_rate(end_date=datetime.datetime.now(datetime.timezone.utc)):
     return None if usd_to_lbp is None else round(usd_to_lbp, 6), None if lbp_to_usd is None else round(lbp_to_usd, 6)
 
 
-def _get_monthly_exchange_rate(month):
+def _get_monthly_exchange_rate(month: datetime.datetime = None):
+    if not month:
+        month = datetime.datetime.now(datetime.timezone.utc)
     usd_to_lbp_rates = []
     lbp_to_usd_rates = []
     cur = month
-    while cur < month + relativedelta(months=1):
+    # works for current date and past ones, because they are in the past
+    while cur < month + relativedelta(months=1) and cur <= datetime.datetime.now(datetime.timezone.utc):
         usd_to_lbp, lbp_to_usd = get_daily_exchange_rate(cur)
         usd_to_lbp_rates.append(usd_to_lbp)
         lbp_to_usd_rates.append(lbp_to_usd)
         cur = cur + datetime.timedelta(days=1)
     try:
-        usd_to_lbp = statistics.mean([x for x in usd_to_lbp_rates if x])
+        usd_to_lbp = statistics.mean([x for x in usd_to_lbp_rates if x is not None])
     except statistics.StatisticsError:
         usd_to_lbp = None
     try:
-        lbp_to_usd = statistics.mean([x for x in lbp_to_usd_rates if x])
+        lbp_to_usd = statistics.mean([x for x in lbp_to_usd_rates if x is not None])
     except statistics.StatisticsError:
         lbp_to_usd = None
     # precision reasons
@@ -70,8 +74,11 @@ def get_daily_exchange_rate(day: datetime.datetime = None):
 
 
 def get_monthly_exchange_rate(month: datetime.datetime = None):
-    if not month or month.month == datetime.datetime.now(datetime.timezone.utc).month:
-        return _get_exchange_rate()
+    if not month or (
+            month.month == datetime.datetime.now(datetime.timezone.utc).month
+            and month.year == datetime.datetime.now(datetime.timezone.utc).year
+    ):
+        return _get_monthly_exchange_rate()
     # any day in the month works
     month = datetime.datetime(month.year, month.month, 1, tzinfo=datetime.timezone.utc)
     item = db.session.query(MonthlyRate).filter_by(date=month.date()).scalar()
@@ -192,13 +199,111 @@ def exchange_rate_trend():
         lbp_to_usd_delta = 100 * (lbp_to_usd - lbp_to_usd_former) / lbp_to_usd_former
     else:
         lbp_to_usd_delta = None
-    return jsonify({"period": period, "usd_to_lbp": {"former_rate": usd_to_lbp_former, "current_rate": usd_to_lbp,
-        "percentage_change": usd_to_lbp_delta,
-        "message": MSG.format("USD-LBP", "risen" if usd_to_lbp_delta > 1e-6 else "fell", round(usd_to_lbp_former, 2),
-                              round(usd_to_lbp, 2), round(abs(usd_to_lbp_delta), 2),
-                              period) if usd_to_lbp_delta is not None else "Rates missing."},
-        "lbp_to_usd": {"former_rate": lbp_to_usd_former, "current_rate": lbp_to_usd,
-            "percentage_change": lbp_to_usd_delta,
-            "message": MSG.format("LBP-USD", "risen" if lbp_to_usd_delta > 1e-6 else "fell",
-                                  round(lbp_to_usd_former, 2), round(lbp_to_usd, 2), round(abs(lbp_to_usd_delta), 2),
-                                  period) if lbp_to_usd_delta is not None else "Rates missing."}})
+    return jsonify({"period": period,
+                    "usd_to_lbp": {
+                        "former_rate": usd_to_lbp_former,
+                        "current_rate": usd_to_lbp,
+                        "percentage_change": usd_to_lbp_delta,
+                        "message": TREND_MSG.format("USD-LBP",
+                                                    "risen" if usd_to_lbp_delta > 1e-6 else "fell",
+                                                    round(usd_to_lbp_former, 2),
+                                                    round(usd_to_lbp, 2),
+                                                    round(abs(usd_to_lbp_delta), 2),
+                                                    period)
+                            if usd_to_lbp_delta is not None else "Rates missing."},
+                    "lbp_to_usd": {
+                        "former_rate": lbp_to_usd_former,
+                        "current_rate": lbp_to_usd,
+                        "percentage_change": lbp_to_usd_delta,
+                        "message": TREND_MSG.format("LBP-USD",
+                                                    "risen" if lbp_to_usd_delta > 1e-6 else "fell",
+                                                    round(lbp_to_usd_former, 2),
+                                                    round(lbp_to_usd,2),
+                                                    round(abs(lbp_to_usd_delta), 2),
+                                                    period)
+                            if lbp_to_usd_delta is not None else "Rates missing."}
+                    })
+
+
+@exchange_bp.route('/exchangeRate/volatility', methods=['GET'])
+@limiter.limit("10 per minute")
+def exchange_rate_volatility():
+    period = request.args.get("period", "").strip()
+    if not period:
+        return jsonify({'error': 'period is required'}), 400
+    end_date = datetime.datetime.now(datetime.timezone.utc)
+    exact = True
+    if period == "24h":
+        start_date = end_date - datetime.timedelta(hours=24)
+    elif period == "7d":
+        start_date = end_date - datetime.timedelta(days=7)
+    elif period == "30d":
+        start_date = end_date - datetime.timedelta(days=30)
+    elif period == "3mon":
+        start_date = end_date - relativedelta(months=3)
+        exact = False
+    elif period == "6mon":
+        start_date = end_date - relativedelta(months=6)
+        exact = False
+    elif period == "1yr":
+        start_date = end_date - relativedelta(years=1)
+        exact = False
+    else:
+        return jsonify({'error': 'period is invalid'}), 400
+    usd_to_lbp_rates = []
+    lbp_to_usd_rates = []
+    while start_date <= end_date:
+        usd_to_lbp, lbp_to_usd = _get_exchange_rate(start_date) if exact else get_daily_exchange_rate(start_date)
+        usd_to_lbp_rates.append(usd_to_lbp)
+        lbp_to_usd_rates.append(lbp_to_usd)
+        start_date += datetime.timedelta(days=1) if period != "24h" else datetime.timedelta(hours=1)
+    usd_to_lbp_rates = [x for x in usd_to_lbp_rates if x is not None]
+    lbp_to_usd_rates = [x for x in lbp_to_usd_rates if x is not None]
+    try:
+        usd_to_lbp_sd = statistics.stdev(usd_to_lbp_rates)
+    except statistics.StatisticsError:
+        usd_to_lbp_sd = None
+    try:
+        lbp_to_usd_sd = statistics.stdev(lbp_to_usd_rates)
+    except statistics.StatisticsError:
+        lbp_to_usd_sd = None
+
+    try:
+        usd_to_lbp_max = max(usd_to_lbp_rates)
+    except ValueError:
+        usd_to_lbp_max = None
+    try:
+        lbp_to_usd_max = max(lbp_to_usd_rates)
+    except ValueError:
+        lbp_to_usd_max = None
+
+    try:
+        usd_to_lbp_min = min(usd_to_lbp_rates)
+    except ValueError:
+        usd_to_lbp_min = None
+    try:
+        lbp_to_usd_min = min(lbp_to_usd_rates)
+    except ValueError:
+        lbp_to_usd_min = None
+    return jsonify({"period": period,
+                    "usd_to_lbp": {
+                        "standard_deviation": usd_to_lbp_sd,
+                        "min_rate": usd_to_lbp_min,
+                        "max_rate": usd_to_lbp_max,
+                        "message": VOLATILITY_MSG.format("USD-LBP",
+                                                         "slightly" if usd_to_lbp_sd < (1000 + 1e-6) else "greatly",
+                                                         round(usd_to_lbp_min, 2),
+                                                         round(usd_to_lbp_max, 2),
+                                                         period)
+                            if usd_to_lbp_sd is not None else "Rates missing."},
+                    "lbp_to_usd": {
+                        "standard_deviation": lbp_to_usd_sd,
+                        "min_rate": lbp_to_usd_min,
+                        "max_rate": lbp_to_usd_max,
+                        "message": VOLATILITY_MSG.format("LBP-USD",
+                                                         "slightly" if lbp_to_usd_sd < (1000 + 1e-6) else "greatly",
+                                                         round(lbp_to_usd_min, 2),
+                                                         round(lbp_to_usd_max, 2),
+                                                         period)
+                            if lbp_to_usd_sd is not None else "Rates missing."}
+                    })
