2bdeaa8afc633ff9a304c23a1f38f1965d7bacdc
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2025-04-16 11:55:43 +0300

    more incorrect query methods

--- api/offer.py
+++ api/offer.py
@@ -76,8 +76,8 @@ def update_offer():
     offer_id = request.json.get('offer_id', None)
     if offer_id is None:
         return jsonify({"error": "Missing required fields"}), 400
-    offer = Offer.query.get(offer_id)
-    if offer is None:
+    offer = db.session.query(Offer).filter_by(id=offer_id).first()
+    if not offer:
         return jsonify({"error": "Invalid offer input"}), 400
     if offer.user_id != int(user.id):
         return jsonify({"error": "Access is forbidden"}), 403
@@ -144,8 +144,8 @@ def accept_offer():
     offer_id = request.json.get('offer_id', None)
     if offer_id is None:
         return jsonify({"error": "Missing required fields"}), 400
-    offer = Offer.query.get(offer_id)
-    if offer is None:
+    offer = db.session.query(Offer).filter_by(id=offer_id).first()
+    if not offer:
         return jsonify({"error": "Invalid offer input"}), 400
     if offer.user_id == int(user.id):
         return jsonify({"error": "Invalid offer input - cannot accept own offer"}), 400
